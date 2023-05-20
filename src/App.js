71251e0a12436ce6b18f1fe65fcdf8b78e4ac803
RyDVi <dmitrii.kardan.rch@gmail.com>
2023-05-20 13:23:40 +0300

    Init app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { jsPDF } from "jspdf";
+import './Arimo-Regular-normal';
 
 function App() {
+  const handleClick = () => {
+    const doc = new jsPDF();
+    doc.setFont("Arimo-Regular", 'normal');
+
+    doc.html(document.body, {
+      callback: function (doc) {
+        doc.save();
+      },
+    });
+  };
   return (
     <div className="App">
+      <p>ololollolol</p>
+      <p>русский текст</p>
+      <button onClick={handleClick}>Print PDF</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
